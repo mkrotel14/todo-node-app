e6ad97c0d2ab05d42466a5115a164ee95137dc3c
Igor Pancheski <igorpraxedeslinux@icloud.com>
2020-03-09 22:27:07 -0300

    added validation of fields in the user registrya

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ Router.post("/new", async (req, res) => {
   try {
     res.json(await UserController.createUser(req.body))
   } catch (e) {
-    res.status(500).json(e.message)
+    res.status(500).send(e.message)
   }
 })
 
@@ -14,16 +14,33 @@ Router.put("/edit", async (req, res) => {
   try {
     res.send(await UserController.updateUser(req.body))
   } catch (e) {
-    res.status(500).json(e.message)
+    res.status(500).send(e.message)
   }
 })
 
-Router.delete("/delete", async (req,res) => {
+Router.delete("/delete", async (req, res) => {
   try {
     res.send(await UserController.deleteUser(req.body))
   } catch (e) {
     res.status(500).json(e.message)
   }
 })
+
+Router.get("/get/:user_id", async (req, res) => {
+  try {
+    res.send(await UserController.getUserByUserId(req.params))
+  } catch (e) {
+    res.status(500).json(e.message)
+  }
+})
+
+Router.get("/get", async (req, res) => {
+  try {
+    res.send(await UserController.getUsers())
+  } catch (e) {
+    res.status(500).json(e.message)
+  }
+})
+
 
 module.exports = Router;
